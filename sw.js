d1d04600df36e0dd03aef649e1734d447ff8ee67
oprasannakumar <30617219+oprasannakumar@users.noreply.github.com>
2026-03-23 18:04:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-const CACHE_NAME = 'billing-dashboard-v1';
+const CACHE_NAME = 'quantum-v1';
 const ASSETS = [
-  './',
-  './index.html'
+  'index.html',
+  'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
